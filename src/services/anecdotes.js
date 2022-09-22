ca51b4e6ffe4c997a3652ee584787de3093d591c
Simo Riihimäki <simo.h.riihimaki@gmail.com>
2022-09-22 12:59:54 +0300

    baseurl korjaus herokua varten

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/anecdotes'
+const baseUrl = 'anecdotes.json'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
